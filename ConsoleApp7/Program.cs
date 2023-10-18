35bbafb1bc59c01cf4db736c4f28553e3b7158b2
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-10-18 20:50:56 +0400

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 52, 25, 56, -45, 23, -12, 24, -91 };
+            int modul = -1;
+            Array(array);
+            ChaneArray(array, modul);
+            Console.WriteLine("Change array:");
+            Array(array);
+
+
+
+
+
+
+        }
+
+        static void ChaneArray(int[] array,int modul)
+        {
+            for(int i=0; i<array.Length; i++)
+            {
+                if (array[i]<0)
+                {
+                    array[i]*=modul;
+                }
+            }
+        }
+        static void Array(int[] array)
+        {
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        
+    }
+}
